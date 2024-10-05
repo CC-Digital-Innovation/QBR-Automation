6f3e8b259fc372d42d6da706f8108e57a1e2a65e
Anthony Farina <farinaanthony96@gmail.com>
2024-10-05 12:10:45 -0400

    trying again

--- src/quarterly_business_report_automation.py
+++ src/quarterly_business_report_automation.py
@@ -18,13 +18,15 @@ load_dotenv(override=True)
 
 # Initialize customer constant global variables.
 # CUSTOMER_CONFIG_JSON = os.getenv('CUSTOMER_CONFIGS')
+# CUSTOMER_CONFIGS = json.loads(CUSTOMER_CONFIG_JSON)
 # print(CUSTOMER_CONFIG_JSON)
 # print(type(CUSTOMER_CONFIG_JSON))
 # if type(CUSTOMER_CONFIG_JSON) is str:
 #     CUSTOMER_CONFIGS = json.loads(CUSTOMER_CONFIG_JSON)
 # else:
 #     CUSTOMER_CONFIGS = CUSTOMER_CONFIG_JSON
-with open('./../customer_configs.json', 'r') as file:
+
+with open('/etc/files/customer_configs.json', 'r') as file:
     CUSTOMER_CONFIGS = json.load(file)
 
 print(CUSTOMER_CONFIGS)
